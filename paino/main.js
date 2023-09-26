a6d2324e37b28d44f0bf3f46ed44eae9bda47d51
thejayadad <thejayadad@gmail.com>
2023-09-26 07:16:41 -0400

    update

--- paino/main.js
+++ paino/main.js
@@ -1,13 +1,13 @@
+
 const keys = document.querySelectorAll('.key');
 const audioElements = document.querySelectorAll('audio');
-
 keys.forEach(key => {
     key.addEventListener('click', () => {
         const note = key.getAttribute('data-note');
         const audio = Array.from(audioElements).find(element => element.getAttribute('data-note') === note);
-        if (audio) {
+        if(audio){
             audio.currentTime = 0;
             audio.play();
         }
-    });
-});
+    })
+})
